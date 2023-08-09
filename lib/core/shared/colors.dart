516909fb536a01f73e6a99ac6d223f675007ad85
Eniola <eniolagboyin@gmail.com>
2023-08-09 15:56:10 +0100

    Colors added

---
+++ lib/core/shared/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color scaffoldColor = Color(0xffF8F8FF);
+  static const Color purple = Color(0xff760BFF);
+  static const Color red = Color(0xffFF3D3D);
+  static const Color orangey = Color(0xffFFC90C);
+  static const Color blue = Color(0xff00AED4);
+  static const Color till= Color(0xffBDBDBD);
+  static const Color textLight = Color(0xff121212);
+  static const Color textDark = Color(0xffFFFFFF);
+  static const Color bgLight = Color(0xffFFFFFF);
+  static const Color bgDark = Color(0xff121212);
+  // static OutlineInputBorder errorBorder = const OutlineInputBorder(
+  //     borderSide: BorderSide(
+  //       color: Colors.red,
+  //     ),
+  //     borderRadius: BorderRadius.all(Radius.circular(20)));
+  // static OutlineInputBorder normalBorder = const OutlineInputBorder(
+  //     borderSide: BorderSide(color: AppColors.textformGrey),
+  //     borderRadius: BorderRadius.all(Radius.circular(20)));
+
+  // static OutlineInputBorder emptyBorder = const OutlineInputBorder(
+  //     borderSide: BorderSide(color: AppColors.textformGrey),
+  //     borderRadius: BorderRadius.all(Radius.circular(20)));
+}
