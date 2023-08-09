00dcad9d67e9e1fa1c559a8c2880e033c4fd257d
Eniola <eniolagboyin@gmail.com>
2023-08-09 16:36:02 +0100

    few icons added

---
+++ lib/core/shared/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const String googleLight = "assets/images/icons/googleLight.svg";
+  static const String googleDark = "assets/images/icons/googleDark.svg";
+  static const String appleLight = "assets/images/icons/appleLight.svg";
+  static const String appleDark = "assets/images/icons/appleDark.svg";
+}
